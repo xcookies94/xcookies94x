1b2af2053d03a8f5c133bb67768ec81f63c86021
xcookies94x <dragonprincess94@hotmail.com>
2011-05-04 15:57:13 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xcookies94x::Application.config.secret_token = 'ad35e2c496c15b3649f3fc542e05a7394871a9bbe0e45be9642e81583af6b98e4cdb07b449ef4ad5753cd7c395a5f78a0293104618e07fe6dfa9289ef5c4e329'
